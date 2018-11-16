d988e8508aafb18cec01fca78cb19276c36cedb6
960761 <275332466@qq.com>
2018-11-16 19:40:30 +0800

    Create reactReduxCompleteDemo.js

---
+++ FreeCodeCamp/FrontEndLibraries/React-Redux/reactReduxCompleteDemo.js
@@ -0,0 +1,231 @@
+
+
+
+
+//react redux complete demo
+/*
+the component you connected to Redux was named Presentational, and this wasn't arbitrary. 
+This term generally refers to React components that are not directly connected to Redux. 
+They are simply responsible for the presentation of UI and do this as a function of the props they receive. 
+By contrast, container components are connected to Redux. 
+These are typically responsible for dispatching actions to the store and often pass store state to child components as props.
+*/
+
+
+
+// Redux:
+const ADD = 'ADD';
+
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message: message
+  }
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(messageReducer);
+
+// React:
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {
+    const currentMessage = this.state.input;
+    this.setState({
+      input: '',
+      messages: this.state.messages.concat(currentMessage)
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+
+// React-Redux:
+const mapStateToProps = (state) => {
+  return { messages: state }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (newMessage) => {
+       dispatch(addMessage(newMessage))
+    }
+  }
+};
+
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
+
+// define the Container component here:
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+class AppWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <Container />
+      </Provider>
+    );
+  }
+};
+
+/******************************************************************************/
+/*
+Now that Redux is connected, you need to extract the state management out of the Presentational component and into Redux. 
+Currently, you have Redux connected, but you are handling the state locally within the Presentational component.
+
+how change:
+In the Presentational component, 
+first, remove the messages property in the local state. These messages will be managed by Redux. 
+Next, modify the submitMessage() method so that it dispatches submitNewMessage() from this.props, 
+   and pass in the current message input from local state as an argument. 
+   Because you removed messages from local state, remove the messages property from the call to this.setState() here as well. 
+Finally, modify the render() method so that it maps over the messages received from props rather than state.
+
+Description:
+Once these changes are made, the app will continue to function the same, except Redux manages the state. 
+This example also illustrates how a component may have local state: your component still tracks user input locally in its own state. 
+You can see how Redux provides a useful state management framework on top of React. 
+You achieved the same result using only React's local state at first, and this is usually possible with simple apps. 
+However, as your apps become larger and more complex, so does your state management, and this is the problem Redux solves.
+
+*/
+
+// Redux:
+const ADD = 'ADD';
+
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message: message
+  }
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(messageReducer);
+
+// React:
+const Provider = ReactRedux.Provider;
+const connect = ReactRedux.connect;
+
+// Change code below this line
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {
+    this.props.submitNewMessage(this.state.input);
+    this.setState({
+      input: ''
+    });   
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.props.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+// Change code above this line
+
+const mapStateToProps = (state) => {
+  return {messages: state}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message))
+    }
+  }
+};
+
+const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational);
+
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Container/>
+      </Provider>
+    );
+  }
+};
